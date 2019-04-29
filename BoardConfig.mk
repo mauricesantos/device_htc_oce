ab66a2ba41ff9c94dc1dba67c96a316bf8631e8e
Siluxsept <DecoyOctopus@gmx.net>
2019-04-29 16:25:33 +0200

    oce: Boardconfig.mk: add DT2W wake node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,6 +142,9 @@ USE_OPENGL_RENDERER := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/htc_sensorhub/sensor_hub/gesture_motion"
+
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
