0c71a968bed6d26684e1d1d5f01037b209472bbb
Siluxsept <DecoyOctopus@gmx.net>
2019-05-27 18:51:13 +0200

    Revert "oce: Add back live display 2.0"
    This reverts commit c93bcead283ebe2aa4b809c64b578a5bcef33c7c.

--- device.mk
+++ device.mk
@@ -348,13 +348,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Marlin IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
