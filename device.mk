0044f074d1bf1d1afce1fe1cb0f5eced487e61a9
Siluxsept <DecoyOctopus@gmx.net>
2019-07-14 18:27:08 +0200

    oce: switch to source built nanoapp_cmd

--- device.mk
+++ device.mk
@@ -448,6 +448,12 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
+    
+PRODUCT_PACKAGES += \
+    nanoapp_cmd
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.common.nanohub.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nanohub.rc
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
