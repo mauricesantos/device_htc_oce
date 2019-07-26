fce7ef81890288f7e20344bd779f55ca92ca0d71
Siluxsept <DecoyOctopus@gmx.net>
2019-07-26 21:13:12 +0200

    Revert "oce: Camera API Override with 26"
    This reverts commit 0ef003d2bdc71a12daed6c304dc750ab076a34a8.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,9 +110,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Camera API Override
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /vendor/bin/mm-qcamera-daemon=26 \
-    /system/vendor/bin/mm-qcamera-daemon=26 \
-    /vendor/bin/hw/android.hardware.camera.provider@2.4-service=26 \
-    /system/vendor/bin/hw/android.hardware.camera.provider@2.4-service=26
+    /system/vendor/bin/mm-qcamera-daemon=26
 
 # Dex
 ifeq ($(HOST_OS),linux)
