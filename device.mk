4a08a7026da2f89a6bd1aa6cc433a6d68032433a
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-11-01 15:06:00 +0100

    oce: add CryptfsHW hidl service
    Change-Id: I6285e456155bfba9671d7609f4b62884af4f8c0f

--- device.mk
+++ device.mk
@@ -217,7 +217,7 @@ PRODUCT_PACKAGES += \
 
 # Encryption
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # Fingerprint
 PRODUCT_PACKAGES += \
